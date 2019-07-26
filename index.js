b5db7710c77c6374f4ba3a49a13fd2d3faa88082
AndriantoAGun <andrianto22491@gmail.com>
2019-07-26 08:27:21 +0700

    Tugas 18 JavaScript
    tugas Niomic tentang JavaScript meliputi modifikasi String
    a. file index : slice (memotong bagian String)
    b. file lndex : concat (menggabungkan String)

---
+++ index.js
@@ -0,0 +1,4 @@
+var str = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+
+var res = str.slice(8, 24);
+console.log(res);
